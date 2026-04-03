8dfa83af9f45684e4adde2c4277df0838be02e53
yuta-toshino1931 <yuta-toshino@hiroba1931.co.jp>
2026-04-03 10:38:51 +0900

    ビルドエラー修正

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/Hometown-Tax-Program/',
   plugins: [react(), tailwindcss()],
 })
